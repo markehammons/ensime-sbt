b5b580376c86d49919dabc24b0b1eafd50a26d4a
Boris Chazalet <boris.chazalet@gmail.com>
2015-11-27 21:59:42 +0000

    Merging all 4 files into a single one

--- src/main/scala/EnsimePlugin.scala
+++ src/main/scala/EnsimePlugin.scala
@@ -403,3 +403,137 @@ object EnsimePlugin extends AutoPlugin with CommandSupport {
   }
 
 }
+
+case class EnsimeConfig(
+  root: File,
+  cacheDir: File,
+  name: String,
+  scalaVersion: String,
+  compilerArgs: List[String],
+  modules: Map[String, EnsimeModule],
+  javaHome: File,
+  javaFlags: List[String],
+  javaSrc: List[File],
+  formatting: Option[IFormattingPreferences]
+)
+
+case class EnsimeModule(
+  name: String,
+  mainRoots: Set[File],
+  testRoots: Set[File],
+  targets: Set[File],
+  testTargets: Set[File],
+  dependsOnNames: Set[String],
+  compileJars: Set[File],
+  runtimeJars: Set[File],
+  testJars: Set[File],
+  sourceJars: Set[File],
+  docJars: Set[File]
+) {
+
+  def dependencies(implicit lookup: String => EnsimeModule): Set[EnsimeModule] =
+    dependsOnNames map lookup
+
+}
+
+trait CommandSupport {
+  this: AutoPlugin =>
+
+  import sbt._
+  import IO._
+
+  protected def fail(errorMessage: String)(implicit state: State): Nothing = {
+    state.log.error(errorMessage)
+    throw new IllegalArgumentException()
+  }
+
+  protected def log(implicit state: State) = state.log
+
+  // our version of http://stackoverflow.com/questions/25246920
+  protected implicit class RichSettingKey[A](key: SettingKey[A]) {
+    def gimme(implicit pr: ProjectRef, bs: BuildStructure, s: State): A =
+      gimmeOpt getOrElse { fail(s"Missing setting: ${key.key.label}") }
+    def gimmeOpt(implicit pr: ProjectRef, bs: BuildStructure, s: State): Option[A] =
+      key in pr get bs.data
+  }
+
+  protected implicit class RichTaskKey[A](key: TaskKey[A]) {
+    def run(implicit pr: ProjectRef, bs: BuildStructure, s: State): A =
+      runOpt.getOrElse { fail(s"Missing task key: ${key.key.label}") }
+    def runOpt(implicit pr: ProjectRef, bs: BuildStructure, s: State): Option[A] =
+      EvaluateTask(bs, key, s, pr).map(_._2) match {
+        case Some(Value(v)) => Some(v)
+        case _              => None
+      }
+  }
+
+}
+
+// direct formatter to deal with a small number of domain objects
+// if we had to do this for general objects, it would make sense
+// to create a series of implicit convertors to an SExp hierarchy
+object SExpFormatter {
+
+  def toSExp(s: String): String =
+    "\"" + s.replace("\\", "\\\\").replace("\"", "\\\"") + "\""
+
+  def toSExp(f: File): String = toSExp(f.getAbsolutePath)
+
+  def fsToSExp(ss: Iterable[File]): String =
+    if (ss.isEmpty) "nil"
+    else ss.map(toSExp).mkString("(", " ", ")")
+
+  def ssToSExp(ss: Iterable[String]): String =
+    if (ss.isEmpty) "nil"
+    else ss.map(toSExp).mkString("(", " ", ")")
+
+  def msToSExp(ss: Iterable[EnsimeModule]): String =
+    if (ss.isEmpty) "nil"
+    else ss.map(toSExp).mkString("(", " ", ")")
+
+  def fToSExp(key: String, op: Option[File]): String =
+    op.map { f => s":$key ${toSExp(f)}" }.getOrElse("")
+
+  def sToSExp(key: String, op: Option[String]): String =
+    op.map { f => s":$key ${toSExp(f)}" }.getOrElse("")
+
+  def toSExp(b: Boolean): String = if (b) "t" else "nil"
+
+  def toSExp(o: Option[IFormattingPreferences]): String = o match {
+    case None                                => ""
+    case Some(f) if f.preferencesMap.isEmpty => ""
+    case Some(f) => f.preferencesMap.map {
+      case (desc, v: Boolean) =>
+        s":${desc.key} ${toSExp(v)}"
+      case (desc, v: Int) =>
+        s":${desc.key} $v"
+    }.mkString(":formatting-prefs (", " ", ")")
+  }
+
+  // a lot of legacy key names and conventions
+  def toSExp(c: EnsimeConfig): String = s"""(
+ :root-dir ${toSExp(c.root)}
+ :cache-dir ${toSExp(c.cacheDir)}
+ :name "${c.name}"
+ :java-home ${toSExp(c.javaHome)}
+ :java-flags ${ssToSExp(c.javaFlags)}
+ :reference-source-roots ${fsToSExp(c.javaSrc)}
+ :scala-version ${toSExp(c.scalaVersion)}
+ :compiler-args ${ssToSExp(c.compilerArgs)}
+ ${toSExp(c.formatting)}
+ :subprojects ${msToSExp(c.modules.values)}
+)"""
+
+  // a lot of legacy key names and conventions
+  def toSExp(m: EnsimeModule): String = s"""(
+   :name ${toSExp(m.name)}
+   :source-roots ${fsToSExp((m.mainRoots ++ m.testRoots))}
+   :targets ${fsToSExp(m.targets)}
+   :test-targets ${fsToSExp(m.testTargets)}
+   :depends-on-modules ${ssToSExp(m.dependsOnNames)}
+   :compile-deps ${fsToSExp(m.compileJars)}
+   :runtime-deps ${fsToSExp(m.runtimeJars)}
+   :test-deps ${fsToSExp(m.testJars)}
+   :doc-jars ${fsToSExp(m.docJars)}
+   :reference-source-roots ${fsToSExp(m.sourceJars)})"""
+}
