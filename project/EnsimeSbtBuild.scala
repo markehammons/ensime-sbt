8f2b04a7a513ddb8983fa8687556ef83b9fc880c
Sam Halliday <sam.halliday@gmail.com>
2016-01-01 12:24:26 +0000

    snapshot

--- project/EnsimeSbtBuild.scala
+++ project/EnsimeSbtBuild.scala
@@ -21,7 +21,7 @@ object EnsimeSbtBuild extends Build {
 
   override val settings = super.settings ++ Seq(
     organization := "org.ensime",
-    version := "0.3.0",
+    version := "0.3.1-SNAPSHOT",
     scalaVersion := "2.10.6",
     ivyLoggingLevel := UpdateLogging.Quiet,
     scalacOptions in Compile ++= Seq(
