6e542e1e1d3d7dfc607453acec2837e500f6182c
Sam Halliday <sam.halliday@gmail.com>
2016-02-20 21:50:50 +0000

    0.4.0

--- project/EnsimeSbtBuild.scala
+++ project/EnsimeSbtBuild.scala
@@ -14,7 +14,7 @@ object EnsimeSbtBuild extends Build {
 
   override val settings = super.settings ++ Seq(
     organization := "org.ensime",
-    version := "0.4.0-SNAPSHOT",
+    version := "0.4.0",
     scalaVersion := "2.10.6"
   ) ++ sonatype("ensime", "ensime-sbt", Apache2)
 
