646d66b269a6a65eb3b50967d8070189894c5f08
Sam Halliday <sam.halliday@gmail.com>
2017-03-03 17:34:27 +0000

    fix the build: workaround bad maven jars

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,6 +1,5 @@
 scalacOptions ++= Seq("-unchecked", "-deprecation")
 ivyLoggingLevel := UpdateLogging.Quiet
-addSbtPlugin("com.fommil" % "sbt-sensible" % "1.1.5")
+addSbtPlugin("com.fommil" % "sbt-sensible" % "1.1.6")
 
 libraryDependencies += "org.scala-sbt" % "scripted-plugin" % sbtVersion.value
-
--- src/sbt-test/sbt-ensime/ensime-server/project/EnsimeBuild.scala
+++ src/sbt-test/sbt-ensime/ensime-server/project/EnsimeBuild.scala
@@ -15,7 +15,10 @@ object EnsimeBuild extends Build {
   lazy override val settings = super.settings ++ Seq(
     scalaVersion := "2.11.8",
     organization := "org.ensime",
-    version := "1.0.0-SNAPSHOT"
+    version := "1.0.0-SNAPSHOT",
+
+    // WORKAROUND bad maven central jar
+    resolvers += "JBoss" at "https://repository.jboss.org/"
   )
 
   lazy val commonSettings = Sensible.settings ++ Seq(
